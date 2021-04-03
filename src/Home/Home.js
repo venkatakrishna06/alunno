7374fa3039c731476032807a171b7b35f6e6bde3
venkatakrishna06 <venkatakrishna9642@gmail.com>
2021-04-03 23:12:45 +0530

    testing home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,8 +1,32 @@
 import React from "react";
+import "./home.css";
+import Logo from "../Document/Logo.png";
 
 function Home() {
     return (
-        <div> Home page</div>
+      <div className="Home">
+        <div id="homePage">
+          <div className="navBar">
+            <ul>
+              <li>
+                <a href="/"><img src={Logo} alt="logo"/></a>
+              </li> 
+              <li>
+                <a href="/Classtt">Class TT</a>
+              </li>
+              <li>
+                <a href="/Assignments">Assignments</a>
+              </li>
+              <li>
+                <a href="/Studymaterial">Studymaterial</a>
+              </li>
+              <li>
+                <a href="/Announcements">Announcements</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     );
 }
 export default Home;
